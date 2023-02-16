c0c20b48c959f900fd0c978b77670e7b24768276
Psiderman <shaun.akash@gmail.com>
2023-02-16 17:28:27 -0500

    KijijiScraper changes
    Fixeed bugs

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ from lxml import etree
 
 result = requests.get('https://www.kijiji.ca/b-cats-kittens/ottawa/kitten/k0c125l1700185')
 tree = etree.HTML(result.text)
-prevLinks = tree.xpath(
-    '/html/body/div[@id="MainContainer"]/div[@id="mainPageContent"]/div[@class="layout-3 "]/div[@class="col-2 "]/main/div/div/@data-vip-url')
+prevLinks = tree.xpath('/html/body/div[@id="MainContainer"]/div[@id="mainPageContent"]/div[@class="layout-3 "]/div[@class="col-2 "]/main/div/div/@data-vip-url')
+
 
 while True:
     result = requests.get('https://www.kijiji.ca/b-cats-kittens/ottawa/kitten/k0c125l1700185')
     tree = etree.HTML(result.text)
     links = tree.xpath(
         '/html/body/div[@id="MainContainer"]/div[@id="mainPageContent"]/div[@class="layout-3 "]/div[@class="col-2 "]/main/div/div/@data-vip-url')
+    print(links[5])
     for listing in links:
         if (listing not in prevLinks):
-            print("ENTER")
-            pywhatkit.sendwhatmsg('+13435429907', listing, datetime.datetime.now().hour,
-                                  datetime.datetime.now().minute + 1)
-            # pywhatkit.sendwhatmsg('+16135136708', listing, datetime.datetime.now().hour, datetime.datetime.now().minute + 1)
+            print('ENTER')
+            pywhatkit.sendwhatmsg('+16135136708', ('https://www.kijiji.ca' + listing), datetime.datetime.now().hour, datetime.datetime.now().minute + 1)
+            # pywhatkit.sendwhatmsg('+16135136708
+
+            # ', listing, datetime.datetime.now().hour, datetime.datetime.now().minute + 1)
 
     prevLinks = links
     time.sleep(60)
+    print("NEWLINE")
